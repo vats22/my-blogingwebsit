93162d096bd1ff7ee2d6c708d73016a536dd98b9
vatsshroff22@gmail.com <vatsshroff22@gmail.com>
2021-04-28 12:18:00 +0530

    footer section

--- JS/main.js
+++ JS/main.js
@@ -19,6 +19,13 @@ $(document).ready(function () {
         navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
     });
 
+    /** CLICK TO SCROLL UP */
+
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+        },2080);
+    })
     
 
 
